3d37cd91d40e4f2b840ff336d76f4c20f26327eb
Pallavi <kul.pallavi01@gmail.com>
2026-03-09 18:35:46 +0530

    test: validate negative salary using full_clean

--- employees/models.py
+++ employees/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 
 class Employee(models.Model):
     full_name = models.CharField(max_length=255)
     job_title = models.CharField(max_length=255)
     country = models.CharField(max_length=100)
-    salary = models.DecimalField(max_digits=12, decimal_places=2)
+    salary = models.DecimalField(
+        max_digits=12,
+        decimal_places=2,
+        validators=[MinValueValidator(0)],
+    )
